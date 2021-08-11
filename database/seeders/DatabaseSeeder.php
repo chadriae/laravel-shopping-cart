b5a791248342068e9425585325c511dfc1b47793
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-08-11 17:37:28 +0200

    Simple shopping cart Laravel example

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        // DB::table('products')->insert([
+        //     'name' => 'Watch',
+        //     'price' => 250,
+        //     'description' => 'Good watch',
+        //     'image' => 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=989&q=80'
+        // ]);
+    }
+}
